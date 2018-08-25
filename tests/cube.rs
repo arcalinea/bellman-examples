f7f37c183e1a32dfe08b99a451b9ae56d62e34b9
Jay Graber <jay@z.cash>
2018-08-24 17:04:18 -0700

    add two examples

---
+++ tests/cube.rs
@@ -0,0 +1,153 @@
+extern crate bellman;
+extern crate pairing;
+extern crate rand;
+
+// For randomness (during paramgen and proof generation)
+use rand::{thread_rng, Rng};
+
+// Bring in some tools for using pairing-friendly curves
+use pairing::{
+    Engine,
+    Field,
+    PrimeField
+};
+
+// We're going to use the BLS12-381 pairing-friendly elliptic curve.
+use pairing::bls12_381::{
+    Bls12,
+    Fr
+};
+
+// We'll use these interfaces to construct our circuit.
+use bellman::{
+    Circuit,
+    ConstraintSystem,
+    SynthesisError
+};
+
+// We're going to use the Groth16 proving system.
+use bellman::groth16::{
+    Proof,
+    generate_random_parameters,
+    prepare_verifying_key,
+    create_random_proof,
+    verify_proof,
+};
+
+// proving that I know x such that x^3 + x + 5 == 35
+// Generalized: x^3 + x + 5 == out
+struct CubeDemo<E: Engine> {
+    x: Option<E::Fr>,
+}
+
+impl <E: Engine> Circuit<E> for CubeDemo<E> {
+    fn synthesize<CS: ConstraintSystem<E>>(
+        self, 
+        cs: &mut CS
+    ) -> Result<(), SynthesisError>
+    {
+        // Flattened into quadratic equations (x^3 + x + 5 == 35): 
+        // x * x = tmp_1
+        // tmp_1 * x = y
+        // y + x = tmp_2
+        // tmp_2 + 5 = out
+        // Resulting R1CS with w = [one, x, tmp_1, y, tmp_2, out]
+        
+        // Allocate the first private "auxiliary" variable
+        let x_val = self.x;
+        let x = cs.alloc(|| "x", || {
+            x_val.ok_or(SynthesisError::AssignmentMissing)
+        })?;
+        
+        
+        // Allocate: x * x = tmp_1
+        let tmp_1_val = x_val.map(|mut e| {
+            e.square();
+            e
+        });
+        let tmp_1 = cs.alloc(|| "tmp_1", || {
+            tmp_1_val.ok_or(SynthesisError::AssignmentMissing)
+        })?;
+        // Enforce: x * x = tmp_1
+        cs.enforce(
+            || "tmp_1",
+            |lc| lc + x,
+            |lc| lc + x,
+            |lc| lc + tmp_1
+        );
+        
+        
+        // Allocate: tmp_1 * x = y
+        let x_cubed_val = tmp_1_val.map(|mut e| {
+            e.mul_assign(&x_val.unwrap());
+            e
+        });
+        let x_cubed = cs.alloc(|| "x_cubed", || {
+            x_cubed_val.ok_or(SynthesisError::AssignmentMissing)
+        })?;
+        // Enforce: tmp_1 * x = y
+        cs.enforce(
+            || "x_cubed",
+            |lc| lc + tmp_1,
+            |lc| lc + x,
+            |lc| lc + x_cubed
+        );
+        
+        // Allocating the public "primary" output uses alloc_input
+        let out = cs.alloc_input(|| "out", || {
+            let mut tmp = x_cubed_val.unwrap();
+            tmp.add_assign(&x_val.unwrap());
+            tmp.add_assign(&E::Fr::from_str("5").unwrap());
+            Ok(tmp)
+        })?;    
+        // tmp_2 + 5 = out
+        // => (tmp_2 + 5) * 1 = out
+        cs.enforce(
+            || "out",
+            |lc| lc + x_cubed + x + (E::Fr::from_str("5").unwrap(), CS::one()),
+            |lc| lc + CS::one(),
+            |lc| lc + out
+        );
+        // lc is an inner product of all variables with some vector of coefficients
+        // bunch of variables added together with some coefficients
+        
+        Ok(())
+    }
+}
+
+#[test]
+fn test_cube_proof(){
+    // This may not be cryptographically safe, use
+    // `OsRng` (for example) in production software.
+    let rng = &mut thread_rng();
+    
+    println!("Creating parameters...");
+    
+    // Create parameters for our circuit
+    let params = {
+        let c = CubeDemo::<Bls12> {
+            x: None
+        };
+
+        generate_random_parameters(c, rng).unwrap()
+    };
+    
+    // Prepare the verification key (for proof verification)
+    let pvk = prepare_verifying_key(&params.vk);
+
+    println!("Creating proofs...");
+    
+    // Create an instance of circuit
+    let c = CubeDemo::<Bls12> {
+        x: Fr::from_str("3")
+    };
+    
+    // Create a groth16 proof with our parameters.
+    let proof = create_random_proof(c, &params, rng).unwrap();
+        
+    assert!(verify_proof(
+        &pvk,
+        &proof,
+        &[Fr::from_str("34").unwrap()]
+    ).unwrap());
+}
---
+++ tests/example.rs
@@ -0,0 +1,130 @@
+extern crate bellman;
+extern crate pairing;
+extern crate rand;
+
+// For randomness (during paramgen and proof generation)
+use rand::{thread_rng, Rng};
+
+// Bring in some tools for using pairing-friendly curves
+use pairing::{
+    Engine,
+    Field,
+    PrimeField
+};
+
+// We're going to use the BLS12-381 pairing-friendly elliptic curve.
+use pairing::bls12_381::{
+    Bls12,
+    Fr,
+};
+
+// We'll use these interfaces to construct our circuit.
+use bellman::{
+    Circuit,
+    ConstraintSystem,
+    SynthesisError
+};
+
+// We're going to use the Groth16 proving system.
+use bellman::groth16::{
+    Proof,
+    generate_random_parameters,
+    prepare_verifying_key,
+    create_random_proof,
+    verify_proof,
+};
+
+// demo circuit
+// proving that I know a such that a * 3 = 21
+struct ProofDemo<E: Engine> {
+    a: Option<E::Fr>,
+    b: Option<E::Fr>,
+    c: Option<E::Fr>
+}
+
+// create a demo circuit by using the `Circuit` trait which
+/// is used during paramgen and proving in order to
+/// synthesize the constraint system.
+impl <E: Engine> Circuit<E> for ProofDemo<E> {
+    fn synthesize<CS: ConstraintSystem<E>>(
+        self, 
+        cs: &mut CS
+    ) -> Result<(), SynthesisError>
+    {
+        
+        // Allocate the first value (private)
+        let a = cs.alloc(|| "a", || {
+            self.a.ok_or(SynthesisError::AssignmentMissing)
+        })?;
+        
+        // Allocate the second value (public)
+        let b = cs.alloc(|| "b", || {
+            self.b.ok_or(SynthesisError::AssignmentMissing)
+        })?;
+        
+        // Allocate the third value (public)
+        // allocating a public input uses alloc_input
+        let c = cs.alloc_input(|| "c", || {
+            self.c.ok_or(SynthesisError::AssignmentMissing)
+        })?;
+
+        // a * b = c?
+        cs.enforce(
+            || "mult",
+            |lc| lc + a,
+            |lc| lc + b,
+            |lc| lc + c
+        );
+        
+        Ok(())
+    }
+}
+
+#[test]
+fn test_proof(){
+    // This may not be cryptographically safe, use
+    // `OsRng` (for example) in production software.
+    let rng = &mut thread_rng();
+    
+    println!("Creating parameters...");
+    
+    // Create parameters for our circuit
+    let params = {
+        let c = ProofDemo::<Bls12> {
+            a: None,
+            b: Fr::from_str("3"),
+            c: Fr::from_str("21")
+        };
+
+        generate_random_parameters(c, rng).unwrap()
+    };
+    
+    // Prepare the verification key (for proof verification)
+    let pvk = prepare_verifying_key(&params.vk);
+
+    println!("Creating proofs...");
+    
+    let out = Fr::from_str("21");
+    
+    // Create an instance of circuit
+    let c = ProofDemo::<Bls12> {
+        a: Fr::from_str("7"),
+        b: Fr::from_str("3"),
+        c: out
+    };
+    
+    // Create a groth16 proof with our parameters.
+    let proof = create_random_proof(c, &params, rng).unwrap();
+    
+    let res = out.unwrap();
+    
+    assert!(verify_proof(
+        &pvk,
+        &proof,
+        &[res]
+    ).unwrap());
+}
+
+
+
+
